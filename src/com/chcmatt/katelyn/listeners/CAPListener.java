d169f13c6090d9e1583d5e3acfe93827e823d370
R2D2Warrior <r2d2warrior@hotmail.com>
2014-03-04 15:27:21 -0500

    Attempt #3

---
+++ src/com/chcmatt/katelyn/listeners/CAPListener.java
@@ -0,0 +1,15 @@
+package com.chcmatt.katelyn.listeners;
+
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.UnknownEvent;
+
+@AddListener
+public class CAPListener extends ListenerAdapter<PircBotX>
+{
+	public void onUnknown(UnknownEvent<PircBotX> event)
+	{
+		if(event.getLine().equals("CAP LS"))
+			event.getBot().sendCAP().request("account-notify", "extended-join");
+	}
+}
