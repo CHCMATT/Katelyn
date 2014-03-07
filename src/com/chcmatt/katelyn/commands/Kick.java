21716d6fe0cb2beff23fb6d6323282f41ca342eb
Matt Anthony <matt@chcmatt.com>
2014-03-07 13:24:27 -0500

    Fix kick messages and take-in Command.java

--- src/com/chcmatt/katelyn/commands/Kick.java
+++ src/com/chcmatt/katelyn/commands/Kick.java
@@ -17,17 +17,19 @@ public class Kick extends GenericCommand
 	public void execute()
 	{
 		String nick = event.getArgumentList().get(0);
+		String sender = event.getUser().getNick();
 		
 		if (userChannelDao.userExists(nick)) // .kick(...) takes a User parameter, not a nick, so make sure the user exists
 		{
 			User user = userChannelDao.getUser(nick); 
 			if (event.getArgumentList().size() == 1) // Cannot use .get(1) with only 1 argument, so check the size instead
 			{
-				channel.send().kick(user); 
+				String reason = "Kick requested by " + sender;
+				channel.send().kick(user, reason); 
 			}
 			else //if event.getArgumentList().size() > 1
 			{
-				String reason = event.getArgRange(1); // .get(1) gets the 2nd argument, .getArgRange(1) gets 2nd to the end
+				String reason = "Kick requested by " + sender + " - " + event.getArgRange(1); // .get(1) gets the 2nd argument, .getArgRange(1) gets 2nd to the end
 				channel.send().kick(user, reason);
 			}
 		}
