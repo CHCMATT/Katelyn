fe59d876c1f9f3926c76c7b4937e8608d772aeaf
R2D2Warrior <r2d2warrior@hotmail.com>
2014-03-15 15:23:59 -0400

    Forgot about this

--- src/com/chcmatt/katelyn/listeners/AddListener.java
+++ src/com/chcmatt/katelyn/listeners/AddListener.java
@@ -1,5 +1,12 @@
 package com.chcmatt.katelyn.listeners;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface AddListener
 {
 	/** Set to false to disable this listener*/
