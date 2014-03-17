70100a6c86d149c265a6b305a0e8886d9b5a944c
Matt Anthony <matt@chcmatt.com>
2014-03-16 22:26:43 -0400

    Initial commit

---
+++ src/com/chcmatt/katelyn/commands/Deop.java
@@ -0,0 +1,39 @@
+package com.chcmatt.katelyn.commands;
+
+import org.pircbotx.Channel;
+import org.pircbotx.Colors;
+import org.pircbotx.PircBotX;
+import org.pircbotx.User;
+
+import com.chcmatt.katelyn.handling.CommandEvent;
+
+@Command(name="deop", desc="Removes channel operator from a user.", syntax = "deop [username]", opOnly=true)
+public class Deop extends GenericCommand
+{
+	
+	public Deop(CommandEvent<PircBotX> event)
+	{
+		super(event);
+	}
+	
+	public void execute()
+	{
+		if (event.hasNoArgs())
+		{
+			channel.send().deOp(userChannelDao.getUser(event.getUser().getNick()));
+		}
+		else 
+		{
+			String nick = event.getArgumentList().get(0);
+			if (userChannelDao.userExists(nick))
+			{
+				User user = userChannelDao.getUser(nick); 
+				channel.send().deOp(user);
+			}
+			else
+			{
+				event.respond(Colors.setBold(user.getNick()) + ": There is no one in "+ Colors.setBold(event.getChannel().getName()) +" with the name " + Colors.setBold(event.getArgumentList().get(0)) + ".");
+			}
+		}
+	}
+}
