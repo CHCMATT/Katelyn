5e2241b10b0345fe8070fab7361baee209e37fc1
Matt Anthony <matt@chcmatt.com>
2014-04-17 01:29:05 -0400

    Add a few more necessary imports

--- src/com/chcmatt/katelyn/commands/Eval.java
+++ src/com/chcmatt/katelyn/commands/Eval.java
@@ -1,8 +1,11 @@
 package com.chcmatt.katelyn.commands;
 
+import org.pircbotx.Colors;
 import org.pircbotx.PircBotX;
+
 import bsh.EvalError;
 import bsh.Interpreter;
+
 
 
 import com.chcmatt.katelyn.utils.Utils;
@@ -11,31 +14,31 @@ import com.chcmatt.katelyn.handling.CommandEvent;
 @Command(name="eval", desc="Evaluate a method within PircBotX", syntax="eval <code to evaluate>", requiresArgs = true, adminOnly=true)
 public class Eval extends GenericCommand
 {
-	
+
 	public Eval(CommandEvent<PircBotX> event)
 	{
 		super(event);
 	}
-	
+
 	@Override
 	public void execute()
 	{
 		String eval = event.getArguments();
-			
+
 		String result = "";
 		try
 		{
 			Interpreter i = Utils.createDefaultInterpreter(event);
-			
+
 			i.eval("item = " + eval);
 			result = user.getNick() + ": "  + i.get("item").toString();
 		}
 		catch (EvalError e)
 		{
-			event.respondToUser("Error occurred during evaluation");
+			event.respondToUser(Colors.setColor("An error occurred during evaluation", Colors.RED));
 			e.printStackTrace();
 		}
-		
+
 		if (!result.toLowerCase().contains("pass") && !eval.toLowerCase().contains("pass"))
 			event.respond(result);
 	}
