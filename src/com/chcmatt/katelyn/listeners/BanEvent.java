03b71f9c0980ab84a609ab9aff95f8f6d6413f17
Matt Anthony <matt@chcmatt.com>
2014-04-30 23:13:39 -0400

    Add BanEvent.java (not working ATM)

---
+++ src/com/chcmatt/katelyn/listeners/BanEvent.java
@@ -0,0 +1,15 @@
+package com.chcmatt.katelyn.listeners;
+
+import org.pircbotx.Colors;
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.SetChannelBanEvent;
+
+@AddListener
+public class BanEvent extends ListenerAdapter<PircBotX>
+{
+	public void onBan(SetChannelBanEvent<PircBotX> event)
+	{
+		event.getBot().sendIRC().action("#Katelyn","Channel ban was set on: "+Colors.setBold(event.getHostmask())+" at "+event.getTimestamp()+" in channel "+event.getChannel());
+	}
+}
