dbede1d01c4b12536fde14ff3b433eafd6910d3c
Matt Anthony <matt@chcmatt.com>
2014-05-01 22:11:40 -0400

    Add fix for BanEvent message to #Katelyn

--- src/com/chcmatt/katelyn/listeners/BanEvent.java
+++ src/com/chcmatt/katelyn/listeners/BanEvent.java
@@ -1,5 +1,8 @@
 package com.chcmatt.katelyn.listeners;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.pircbotx.Colors;
 import org.pircbotx.PircBotX;
 import org.pircbotx.hooks.ListenerAdapter;
@@ -8,8 +11,9 @@ import org.pircbotx.hooks.events.SetChannelBanEvent;
 @AddListener
 public class BanEvent extends ListenerAdapter<PircBotX>
 {
+	@Override
 	public void onSetChannelBan(SetChannelBanEvent<PircBotX> event)
 	{
-		event.getBot().sendIRC().action("#Katelyn","Channel ban was set on: "+Colors.setBold(event.getHostmask())+" at "+event.getTimestamp()+" in channel "+event.getChannel());
+		event.getBot().sendIRC().action("#Katelyn","Channel ban was set on: "+Colors.setBold(event.getHostmask())+" at "+new SimpleDateFormat(Colors.setBold("h:mm:ss a")).format(new Date())+" in channel "+Colors.setBold(event.getChannel().getName()));
 	}
 }
