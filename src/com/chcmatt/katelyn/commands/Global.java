f9d1a9b18811e79778831ef9dca46a07e11e413f
Matt Anthony <matt@chcmatt.com>
2014-05-16 00:47:58 -0400

    Add/fix the initial global command.

--- src/com/chcmatt/katelyn/commands/Global.java
+++ src/com/chcmatt/katelyn/commands/Global.java
@@ -1,21 +1,36 @@
 package com.chcmatt.katelyn.commands;
 
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.StringUtils;
+import org.pircbotx.Channel;
 import org.pircbotx.Colors;
 import org.pircbotx.PircBotX;
 
 import com.chcmatt.katelyn.handling.CommandEvent;
-@Command(name="global", alias={"g","gl"}, desc="Sends a global command.", syntax="global <subCommand> [sumcommandParameters]", requiresArgs=true, adminOnly=true)
+@Command(name="global", alias={"g","gl"}, desc="Sends a global command.", syntax="global <subCommand> [sumCommandParameters]", requiresArgs=true, adminOnly=true)
 public class Global extends GenericCommand
-{
+{ 
 	
 	public Global(CommandEvent<PircBotX> event)
 	{
 		super(event);
 	}
+	@Command.Default
+	public void missingMessage()
+	{
+		event.respondToUser(Colors.setColor(Colors.setBold("Error:")+" This command requires arguments. "+Colors.setBold("SYNTAX:")+" global <subCommand> [sumCommandParameters]",Colors.RED));
+	}
 	@Command.Sub(name="channels", alias={"c","ch","chan","chans"})
 	public void globalChannels()
 	{
-		event.respond(user.getNick() + ": " + bot.getUserBot().getChannels().toString());
+		ArrayList<String> chanNames = new ArrayList<>();
+		 
+		for (Channel chan : bot.getUserBot().getChannels())
+		    chanNames.add(chan.getName());
+		 
+		String chans = StringUtils.join(chanNames, ", ");
+		event.respond(user.getNick()+": "+chans);
 	}
 	
 	@Command.Sub(name="sub2", adminOnly=true)
