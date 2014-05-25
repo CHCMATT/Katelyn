254f11e656080512334fb559bb6493822c5c62de
Matt Anthony <matt@chcmatt.com>
2014-05-25 18:06:44 -0400

    Add checks for making help command display all commands properly.

--- src/com/chcmatt/katelyn/commands/Help.java
+++ src/com/chcmatt/katelyn/commands/Help.java
@@ -28,12 +28,41 @@ public class Help extends GenericCommand
 		if (event.hasNoArgs())
 		{
 			event.respond(Colors.setBold("Valid prefixes: ") + "\"" + prePublic + "\" to public message, \"" + prePrivate +"\" to private notice.");
+			//Create ArrayList for each set of commands
 			List<String> commandList = new ArrayList<String>();
+			List<String> modCommandList = new ArrayList<String>();
+			List<String> adminCommandList = new ArrayList<String>();
+			List<String> ownerCommandList = new ArrayList<String>();
+			
 			for (CommandInfo<GenericCommand> info : bot.getCommandRegistry().getCommands())
-				commandList.add(info.getName());
-
+			{
+				//check if user is mod
+				if (event.getUser().getGroup().getName().equals("mod"))
+					modCommandList.add(info.getName());
+				//checks if user is admin
+				else if (event.getUser().getGroup().getName().equals("admin"))
+					adminCommandList.add(info.getName());
+				//checks if user is owner
+				else if (event.getUser().getGroup().getName().equals("owner"))
+					ownerCommandList.add(info.getName());
+				else
+					commandList.add(info.getName());
+			}
+			//Organizes the commands into alphabetical order
 			Collections.sort(commandList);
+			Collections.sort(modCommandList);
+			Collections.sort(adminCommandList);
+			Collections.sort(ownerCommandList);
+			
 			event.respond(Colors.setBold("Commands: ") + StringUtils.join(commandList, ", "));
+			if (event.getUser().getGroup().getName().equals("mod") || bot.getPermissions().getGroupNames().contains("admin") || bot.getPermissions().getGroupNames().contains("owner"))
+				event.respondToUser(Colors.setBold("Moderator only Commands: ") + StringUtils.join(modCommandList, ", "));
+			
+			if (bot.getPermissions().getGroupNames().contains("admin") || bot.getPermissions().getGroupNames().contains("owner"))
+				event.respondToUser(Colors.setBold("Admin only Commands: ") + StringUtils.join(adminCommandList, ", "));
+			
+			if (bot.getPermissions().getGroupNames().contains("owner"))
+				event.respondToUser(Colors.setBold("Owner only Commands: ") + StringUtils.join(ownerCommandList, ", "));
 		}
 		else
 		{
@@ -44,7 +73,7 @@ public class Help extends GenericCommand
 				event.respond(cmd.toLowerCase() + " - " + desc);
 			}
 			else
-				event.respondToUser("Sorry, I don't have a command \"" + cmd + "\".");
+				event.respondToUser("Sorry, I don't have a command called" + cmd + ".");
 		}
 	}
 }
