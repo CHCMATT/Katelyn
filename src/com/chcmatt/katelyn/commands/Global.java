c6edc474b23caec28315c592c20f4448e0739f3c
Matt Anthony <matt@chcmatt.com>
2014-05-29 23:22:28 -0400

    Add message subcommand

--- src/com/chcmatt/katelyn/commands/Global.java
+++ src/com/chcmatt/katelyn/commands/Global.java
@@ -21,6 +21,15 @@ public class Global extends GenericCommand
 	{
 		event.respondToUser(Colors.setColor(Colors.setBold("Error:")+" This command requires arguments. "+Colors.setBold("SYNTAX:")+" global <subCommand> [sumCommandParameters]",Colors.RED));
 	}
+	@Command.Sub(name="message", alias={"m","msg"}, requiresArgs=true, syntax="message <message>", minGroup="admin")
+	public void globalMessage()
+	{
+		String message = event.getArguments();
+		 
+		for (Channel chan : bot.getUserBot().getChannels())
+		    chan.send().message("["+Colors.setBold("Global")+"] "+Colors.setBold(user.getNick())+": "+message);;
+	}
+	
 	@Command.Sub(name="channels", alias={"c","ch","chan","chans"})
 	public void globalChannels()
 	{
